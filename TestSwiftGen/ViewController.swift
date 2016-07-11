5bb6b8c692b47e3320040a753f888750b53b5627
Suraphan Laokondee <suraphan.l@kaidee.com>
2016-07-12 00:18:05 +0700

    Add StoryboardAutoGen

--- TestSwiftGen/ViewController.swift
+++ TestSwiftGen/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    navigationController?.pushViewController(StoryboardScene.Main.TestAutoGenScene.viewController(), animated: true)
   }
 
   override func didReceiveMemoryWarning() {
